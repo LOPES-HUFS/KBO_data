2bd6aeb4e8faedf12679c3f619790ef4bd59112f
Jaehwan <dialektike@gmail.com>
2021-02-19 17:15:15 +0900

    reformatted with black

--- kbo_data/get_data.py
+++ kbo_data/get_data.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-from pasing_page import scoreboard,etc_info,looking_for_team_name
-from pasing_page import away_batter,home_batter,away_pitcher,home_pitcher
+from pasing_page import scoreboard, etc_info, looking_for_team_name
+from pasing_page import away_batter, home_batter, away_pitcher, home_pitcher
 
 
 config = configparser.ConfigParser()
