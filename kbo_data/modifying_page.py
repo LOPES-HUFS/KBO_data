abf5bb662800ce547cfbeaaa2433e7bcc4b90c5b
Jaehwan <dialektike@gmail.com>
2021-05-01 01:23:21 +0900

    스코어보드 정리 코드 작성 시작

--- kbo_data/modifying_page.py
+++ kbo_data/modifying_page.py
@@ -1,5 +1,23 @@
-""" 가져온 KBO 자료를 DB에 저장하기 위한 모듈
+""" 가져온 KBO 자료를 적절하게 정리하고 DB나 파일로 저장하기 위한 모듈
 
+
+```python
+    import json
+    file_name = "2021_04.29_games.json"
+    temp_data = {}
+    with open(file_name) as json_file:
+            temp_data = json.load(json_file)
+    import modifying_page
+    temp = modifying_page.input_data(temp_data)
+    temp
+    ```
 """
 
 import json
+
+import scoreboards
+
+def input_data(data):
+    temp_data = { }
+    temp_data['scoreboards'] = scoreboards.input_data(data)
+    return temp_data
