9c81f9a8b34cd7b9b363c5d9e535f5947992748e
Jaehwan <dialektike@gmail.com>
2021-05-01 01:37:20 +0900

    연습용 파일 다운 받기 설명 추가

--- kbo_data/modifying_page.py
+++ kbo_data/modifying_page.py
@@ -1,6 +1,15 @@
 """ 가져온 KBO 자료를 적절하게 정리하고 DB나 파일로 저장하기 위한 모듈
 
 
+연습용 json 파일 다운받기
+
+```
+wget https://gist.github.com/dialektike/\
+761a6053b0c231316a87818aec87a4a7/raw/2021_04.29_games.json
+```
+
+사용 방법
+
 ```python
     import json
     file_name = "2021_04.29_games.json"
