6b7199b8a002c7f4ce280e4716d92488297de52f
Jess Ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2021-08-24 21:59:22 +0900

    Update create_table.sql

--- schema/poc/create_table.sql
+++ schema/poc/create_table.sql
@@ -42,16 +42,36 @@ CREATE TABLE `scoreboard` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*batters*/
-CREATE TABLE batters(
-    idx INT NOT NULL,
-    playerid INT NOT NULL,
-    position VARCHAR(20) NOT NULL,
-    batnum INT(10) NOT NULL,
-    hit INT(10) NOT NULL,
-    CONSTRAINT scoreboards_batteridx_fk FOREIGN KEY (idx) REFERENCES scoreboards (idx),
-    CONSTRAINT PlayerID_batter_fk FOREIGN KEY (playerid) REFERENCES PlayerID (playerid)
+CREATE TABLE batter(
+    idx BIGINT(11) NOT NULL,
+    playerid INT(5) NOT NULL,
+    position CHAR(1) DEFAULT NULL,
+    i_1 INT(8) DEFAULT NULL,
+    i_2  INT(8) DEFAULT NULL,
+    i_3  INT(8) DEFAULT NULL,
+    i_4  INT(8) DEFAULT NULL,
+    i_5  INT(8) DEFAULT NULL,
+    i_6  INT(8) DEFAULT NULL,
+    i_7  INT(8) DEFAULT NULL,
+    i_8  INT(8) DEFAULT NULL,
+    i_9  INT(8) DEFAULT NULL,
+    i_10  INT(8) DEFAULT NULL,
+    i_11  INT(8) DEFAULT NULL,
+    i_12  INT(8) DEFAULT NULL,
+    i_13  INT(8) DEFAULT NULL,
+    i_14  INT(8) DEFAULT NULL,
+    i_15  INT(8) DEFAULT NULL,
+    i_16  INT(8) DEFAULT NULL,
+    i_17  INT(8) DEFAULT NULL,
+    i_18  INT(8) DEFAULT NULL,
+    hit  INT(2) DEFAULT NULL,
+    bat_num  INT(2) DEFAULT NULL,
+    hit_prob  DECIMAL(6,3) DEFAULT NULL,
+    hit_get  INT(2) DEFAULT NULL,
+    own_get  INT(2) DEFAULT NULL,
+    CONSTRAINT scoreboards_batter_idx_fk FOREIGN KEY (idx) REFERENCES scoreboard (idx),
+    CONSTRAINT player_id_batter_playerid_fk FOREIGN KEY (playerid) REFERENCES player_id (playerid)
 );
-
 
 /*pitchers*/
 CREATE TABLE pitchers(
