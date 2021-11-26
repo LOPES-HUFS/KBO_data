ee52a09ec30c55e3868ac6b365b39c64e4f65eb9
Jess <42489770+Hyeonji-Ryu@users.noreply.github.com>
2021-11-26 20:43:16 +0900

    Update for_actions.py

--- kbo_data/for_actions.py
+++ kbo_data/for_actions.py
@@ -52,9 +52,12 @@ if __name__ == "__main__":
             print(e)
 
     print("자료 정리 시작")
+    config = configparser.ConfigParser()
+    config.read("code_list.ini", encoding="utf-8")
+   
     temp_scoreboards = scoreboards.output_to_dict(game_date)
     temp_pitchers = pitchers.output(game_date)
-    temp_batters = batters.output(game_date)
+    temp_batters = batters.output(config["BATTER"], game_date)
     print("자료 정리 완료 & 정리한 자료 보기")
     print(temp_scoreboards)
     print(temp_pitchers)
